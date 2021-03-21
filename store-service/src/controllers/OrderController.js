6ef4055dad81e6acd3749c9c27ad5b4669b98cd9
ianito <iaan.developer@gmail.com>
2021-03-21 03:03:00 -0300

    Co-authored-by: lavininhamelo@hotmail.com <lavininhamelo@hotmail.com>

--- store-service/src/controllers/OrderController.js
+++ store-service/src/controllers/OrderController.js
@@ -1,28 +1,28 @@
-import Order from '../models/Order'
+import Order from '../models/Order';
 
 class OrderController {
   async index(req, res) {
-    const orders = await Order.find({})
-    return res.json(orders)
+    const orders = await Order.find({});
+    return res.json(orders);
   }
 
   async show(req, res) {
-    const { id } = req.params
+    const { id } = req.params;
     try {
-      const order = await Order.findById(id)
+      const order = await Order.findById(id);
 
       if (!order) {
-        return res.status(404).json({ error: 'Order was not found!' })
+        return res.status(404).json({ error: 'Order was not found!' });
       }
 
-      return res.status(200).json(order)
+      return res.status(200).json(order);
     } catch (err) {
-      return res.status(400).json({ error: 'Error retrieving data!' })
+      return res.status(400).json({ error: 'Error retrieving data!' });
     }
   }
 
   async create(req, res) {
-    const { name, quantity, session, user, value, date, status } = req.body
+    const { name, quantity, session, user, value, date, status } = req.body;
 
     const order = new Order({
       name,
@@ -31,34 +31,34 @@ class OrderController {
       user,
       value,
       date,
-      status
-    })
+      status,
+    });
 
     await order.save(function (err, order) {
-      if (err) return console.log('ERRO', err)
-      res.send({ data: order, message: 'Order was created successfully' })
-    })
+      if (err) return console.log('ERRO', err);
+      res.send({ data: order, message: 'Order was created successfully' });
+    });
   }
 
   async delete(req, res) {
-    const { id } = req.params
+    const { id } = req.params;
 
     try {
-      const order = await Order.findById(id)
+      const order = await Order.findById(id);
 
       if (!order) {
-        return res.status(404).json({ error: 'Order was not found!' })
+        return res.status(404).json({ error: 'Order was not found!' });
       }
 
-      await order.remove()
+      await order.remove();
 
       return res
         .status(204)
-        .json({ message: 'Order annotation has been deleted!' })
+        .json({ message: 'Order annotation has been deleted!' });
     } catch (err) {
-      return res.status(400).json({ error: 'Error deleting data!' })
+      return res.status(400).json({ error: 'Error deleting data!' });
     }
   }
 }
 
-export default new OrderController()
+export default new OrderController();
